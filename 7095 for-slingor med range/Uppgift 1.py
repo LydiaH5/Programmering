3bdf6ea9f8e2aae768a0494478b61c7bda3c8421
Lydia Hammarlund <lydia.hammarlund05@skola.stenungsund.se>
2023-10-23 11:06:22 +0200

    Klar

---
+++ 7095 for-slingor med range/Uppgift 1.py
@@ -0,0 +1,16 @@
+for i in range(0, 5):
+    print(i)
+print("Klart")
+for i in range(3, 8):
+    print(i)
+print("Klart")
+for i in range(4, 20, 3):
+    print(i)
+print("Klart")
+for i in range(20, 41, 5):
+    print(i)
+print("Klart")
+for i in range(10, -1, -1):
+    print(i)
+print("Klart")
+    
---
+++ 7096 for each-slingor/Uppgift 1-2.py
@@ -0,0 +1,12 @@
+text = "1001011"
+antal1 = 0
+for siffra in text:
+    if siffra == "1":
+        antal1 = antal1 + 1
+print("Antal ettor i 1001011:", antal1)
+text2 = "13324343532611465515256554532451154255241636666454"
+antal6 = 0
+for siffra in text2:
+    if siffra == "6":
+        antal6 = antal6 + 1
+print("Antal sexor i 13324343532611465515256554532451154255241636666454:", antal6)

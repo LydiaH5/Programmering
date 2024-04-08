c9d72f9405c566acd9599167374cb066773b78d7
Lydia Hammarlund <lydia.hammarlund05@skola.stenungsund.se>
2024-04-08 11:10:34 +0200

    pågående

--- 7100 listor/Uppgift_8_version2.py
+++ 7100 listor/Uppgift_8_version2.py
@@ -6,42 +6,43 @@ for i in range(16):
     p.append(0)
 runda = 0
 kategorier = []
-kategorier2 = [0, Ettor, Tvåor, Treor, Fyror, Femmor, Sexor, Par, Tvåpar, Tretal, Fyrtal, Kåk, Liten stege, Stor stege, Chans, Yatzy]
+kategorier2 = [0, "Ettor", "Tvåor", "Treor", "Fyror", "Femmor", "Sexor", "Par", "Tvåpar", "Tretal", "Fyrtal", "Kåk", "Liten stege", "Stor stege", "Chans", "Yatzy"]
 spela = input("Yatzy! Skriv s för att påbörja spel.\n")
 while spela == "s":
     runda = runda + 1
-    noresult = 0
+    delrunda = 0
     tärningar = []
     for i in range(5):
         tärningar.append(random.randint(1, 6))
     print("Dina första 5 tärningar blev:", tärningar)
-    kasta = input("Välj tärningar från 1-5 att kasta eller 0 för att behålla alla.\n")    
-    kast = []
-    for char in kasta:
-        if char.isdigit():
-            kast.append(int(char))
-    if 0 in kast:
-        print("")
-    else:
-        for i in kast:
-            tärningar.pop(i-1)
-            tärningar.insert(i-1, random.randint(1, 6))
-    print("Dina tärningar är nu", tärningar)
-    kasta = input("Välj tärningar från 1-5 att kasta eller 0 för att behålla alla.\n")    
-    kast = []
-    for char in kasta:
-        if char.isdigit():
-            kast.append(int(char))
-    if 0 in kast:
-        print("")
-    else:
-        for i in kast:
-            tärningar.pop(i-1)
-            tärningar.insert(i-1, random.randint(1, 6))
-    print("Dina tärningar är nu", tärningar)
-    #testa vilka placera som går:
+    while delrunda < 2: 
+        kasta = input("Välj tärningar från 1-5 att kasta eller 0 för att behålla alla.\n")    
+        kast = []
+        for char in kasta:
+            if char.isdigit():
+                kast.append(int(char))
+        if 0 in kast:
+            print("")
+        else:
+            for i in kast:
+                tärningar.pop(i-1)
+                tärningar.insert(i-1, random.randint(1, 6))
+        print("Dina tärningar är nu", tärningar)
+        delrunda = delrunda + 1
 
-    if noresult == 1: #if inget resultat passar:
+    #testa vilka placera som går:
+    tärningar.sort()
+    noresult = [0]
+    for i in "123456": 
+        if i not in tärningar or i in kategorier: 
+            noresult.append(int(i)) 
+            #(lägger till 1-6 i noresult)
+    for i in tärningar:
+        n = tärningar.count
+        if n > 1:
+            par = "true" 
+            #(tittar om det finns par)
+    if sum(noresult) == 120: #if inget resultat passar:
         print("Ditt resultat passar  inte någonstans! Välj en kategori att stryka:\n\nEttor (1) (", p[1], "poäng)\nTvåor (2) (", p[2], "poäng)\nTreor (3) (", p[3], "poäng)\nFyror (4) (", p[4], "poäng)\nFemmor (5) (", p[5], "poäng)\nSexor (6) (", p[6], "poäng)\nEtt par (7) (", p[7], "poäng)\nTvå par (8) (", p[8], "poäng)\nTretal (9) (", p[9], "poäng)\nFyrtal (10) (", p[10], "poäng)\nLiten stege (11) (", p[11], "poäng)\nStor stege (12) (", p[12], "poäng)\nKåk (13) (", p[13], "poäng)\nChans (14) (", p[14], "poäng)")
         släng = int(input("Yatzy (15) (", p[15], "poäng)\n"))
         p[släng] = 0
@@ -53,9 +54,13 @@ while spela == "s":
     while placera != 0:
         if placera in kategorier:
             placera = input("Kategorin", kategorier2[placera], "är upptagen. Välj en annan kategori.\n")
-        if 1 == 1: #ändra till if placera ej är möjligt
+        elif placera in noresult:
             placera = input("Du har inga", kategorier2[placera], "att placera! Välj en annan kategori.\n")
         else:
+            if placera < 7:
+                for tärning in tärningar:
+                    if tärning == placera:
+                        p[placera] = p[placera] + placera 
         #ge poäng
             kategorier.append(placera) 
             poäng = poäng + p[placera]
@@ -65,5 +70,5 @@ while spela == "s":
         if sum(kategorier) == 120:
                 print("Ditt protokoll är fyllt! Spelet är nu avslutat. Ditt resultat:", poäng, "poäng!") 
         else:
-            print("Runda", runda, "avslutad. Du har nu", p[placera], "poäng i kategorin", kategorier2[placera] "och dina totala poäng är:", poäng)
+            print("Runda", runda, "avslutad. Du har nu", p[placera], "poäng i kategorin", kategorier2[placera], "och dina totala poäng är:", poäng)
             spela = input("Skriv s för att påbörja nästa runda.\n")
